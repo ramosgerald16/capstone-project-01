b381e6f437fe316b2c332fcd958534a78f8fe684
Gerald Ramos <33362256+ramosgerald16@users.noreply.github.com>
2023-07-26 19:47:38 +0800

    removed default css files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <card>
+      <p>Test</p>
+      <button>This is a sample button</button>
+      </card>
     </>
   )
 }
